3d328fc08be18ea065e2fd633df0b6a26d6acf36
Leon Li <qianthinking@gmail.com>
2012-02-21 12:49:30 +0800

    fix wget usage

--- install_all_noi.sh
+++ install_all_noi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://raw.github.com/qianthinking/dotfiles/master/install_all.sh /tmp/install_all.sh
+wget https://raw.github.com/qianthinking/dotfiles/master/install_all.sh -O /tmp/install_all.sh
 chmod a+x /tmp/install_all.sh
 /tmp/install_all.sh
