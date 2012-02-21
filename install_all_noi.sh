aad7a7700195dce13fc11441e081748d7d980aa6
Leon Li <qianthinking@gmail.com>
2012-02-21 13:09:18 +0800

    require interactive shell to execute install script with customization

--- install_all_noi.sh
+++ install_all_noi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://raw.github.com/qianthinking/dotfiles/master/install_all.sh -O /tmp/install_all.sh
+wget https://raw.github.com/qianthinking/dotfiles/master/install_all_with_interactive_shell.sh -O /tmp/install_all.sh
 chmod a+x /tmp/install_all.sh
-/tmp/install_all.sh
+echo "please execute `tput setaf 2; tput bold`/tmp/install_all.sh`tput sgr0` manually"
