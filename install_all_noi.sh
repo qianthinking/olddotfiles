093f346876d2846a7b9949c557f10308154cfdbe
Leon Li <qianthinking@gmail.com>
2012-02-21 17:46:32 +0800

    better prompt after init installation

--- install_all_noi.sh
+++ install_all_noi.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 wget https://raw.github.com/qianthinking/dotfiles/master/install_all_with_interactive_shell.sh -O /tmp/install_all.sh
 chmod a+x /tmp/install_all.sh
+
 echo "please execute `tput setaf 2; tput bold`/tmp/install_all.sh`tput sgr0` manually"
+
+
