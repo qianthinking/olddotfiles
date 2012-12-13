75bdd68de2f4ae4c0b154da3e9665bcaca297542
Leon Li <qianthinking@gmail.com>
2012-12-13 12:52:26 +0800

    ensure bash work

--- install_all_with_interactive_shell.sh
+++ install_all_with_interactive_shell.sh
@@ -34,13 +34,13 @@ if [ -d "$INSTALL_DIR/git-prompt" ]; then
 else
     cd $INSTALL_DIR
     git clone git://github.com/qianthinking/git-prompt.git
-    if ! grep "git-prompt.sh" ~/.bash_profile; then
-        echo "[[ \$- == *i* ]] && . $INSTALL_DIR/git-prompt/git-prompt.sh" >> ~/.bash_profile
-    fi
-    if ! grep "git-prompt.sh" ~/.bashrc; then
-        echo "[[ \$- == *i* ]] && . $INSTALL_DIR/git-prompt/git-prompt.sh" >> ~/.bashrc
-    fi
     ln -fns $INSTALL_DIR/git-prompt/git-prompt.conf ~/.git-prompt.conf
+fi
+if ! grep "git-prompt.sh" ~/.bash_profile; then
+    echo "[[ \$- == *i* ]] && . $INSTALL_DIR/git-prompt/git-prompt.sh" >> ~/.bash_profile
+fi
+if ! grep "git-prompt.sh" ~/.bashrc; then
+    echo "[[ \$- == *i* ]] && . $INSTALL_DIR/git-prompt/git-prompt.sh" >> ~/.bashrc
 fi
 
 #install tmux
@@ -50,12 +50,12 @@ if [ -d "$INSTALL_DIR/dotfiles" ]; then
 else
     cd $INSTALL_DIR
     git clone git://github.com/qianthinking/dotfiles.git
-    if ! grep "bashrc.addon" ~/.bashrc; then
-        echo ". $INSTALL_DIR/dotfiles/.bashrc.addon" >> ~/.bashrc
-    fi
     install_script ".gitconfig"
     install_script ".pentadactylrc"
     install_script ".tmux.conf"
+fi
+if ! grep "bashrc.addon" ~/.bashrc; then
+    echo ". $INSTALL_DIR/dotfiles/.bashrc.addon" >> ~/.bashrc
 fi
 
 if [ -d "$INSTALL_DIR/dotvim" ]; then
